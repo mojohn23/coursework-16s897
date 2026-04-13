21f65f7565edc52773aa456b55d5a3ab2a7462a1
mojohn23 <157812216+mojohn23@users.noreply.github.com>
2026-04-12 23:24:07 -0400

    Updated for fulfill PSET 3 part 2 and sub parts

--- PsycheScripts/attitude_estimation.py
+++ PsycheScripts/attitude_estimation.py
@@ -5,6 +5,8 @@ import time
 import matplotlib.pyplot as plt
 import adcs_toolbox as adcs
 
+H = np.vstack([np.zeros((1, 3)), np.eye(3)])
+
 ##### Part 4: Wahba's Problem: Wahba's Solver Functions ##################################
 S_SRU = np.array([[4.85e-6**2, 0, 0], [0, 4.85e-6**2, 0], [0, 0, 3.88e-5**2]])
 S_CSS = np.array([[0.017**2, 0, 0], [0, 0.017**2, 0], [0, 0, 0.017**2]])
@@ -12,22 +14,29 @@ sensors = 4 # Assume 2 SRU measurements and 2 CSS measurements
 w = np.array([4.85e-6**-2, 4.85e-6**-2, 0.017**-2, 0.017**-2]) # Initialize weights
 S = [S_SRU, S_SRU, S_CSS, S_CSS]
 
-def random_sensor(sensors):
+def random_sensor(n_SRU, n_CSS):
     # Generate random "ground truth" attitude
     q_real = adcs.qexp(np.random.randn(3)) # Use randn (normal distribution) not rand!!
     Q_real = adcs.Q(q_real)
 
-    # Generate random "sensor readings" in the body frame and normalize as unit vec
-    r_B = np.random.randn(3, sensors)
-    for i in range(sensors):
-        r_B[:, i] = adcs.unit_vec(r_B[:, i])
-    r_N_real = Q_real @ r_B # Perfect sensor readings in the inertial frame
+    r_B = np.zeros((3, n_CSS + n_SRU))
+    r_N_real = np.zeros((3, n_CSS + n_SRU))
 
-    # Add noise to the sensor readings in the body frame and renormalize as unit vec
-    for i in range(sensors):
-        r_B[:, i] = r_B[:, i] + np.random.multivariate_normal(np.zeros(3), S[i])
-        r_B[:, i] = adcs.unit_vec(r_B[:, i])
-    # r_B becomes the noisy sensor readings in the body frame
+    # Generate random SRU measurements: multiplicative error
+    for i in range(n_SRU):
+        r_B[:, i] = adcs.unit_vec(np.random.randn(3)) # Generate random inertial unit vector in the body frame
+        r_N_real[:, i] = adcs.unit_vec(Q_real.T@r_B[:, i]) # Transform real vector to inertial frame, real inertial frame
+        noise_vec = np.random.multivariate_normal(np.zeros(3), S_SRU) # Sample noise vector as 3-param
+        delta_q = adcs.qexp(noise_vec) # Turn into noise quaternion
+        q_noisy = adcs.qmult(q_real, delta_q) # Multiply the error to get the noisy quaternion
+        r_B[:, i] = adcs.unit_vec(adcs.Q(q_noisy).T@r_N_real[:, i]) # With noisy quaternion, produce noisy measurement in the body frame
+
+# Generate random CSS measurements: additive error
+    for i in range (n_CSS):
+        r_B[:, n_SRU + i] = adcs.unit_vec(np.random.randn(3)) # Generate random inertial unit vector in the body frame
+        r_N_real[:, n_SRU + i] = adcs.unit_vec(Q_real.T@r_B[:, n_SRU + i]) # Transform real vector to inertial frame, real inertial frame
+        r_B[:, n_SRU + i] = adcs.unit_vec(r_B[:, n_SRU + i] + np.random.multivariate_normal(np.zeros(3), S_CSS)) # r_B = r_B_real + noise, overwrite r_B and normalize
+
     return r_B, r_N_real, Q_real, q_real
 
 # #Convex Relaxation Method
@@ -55,8 +64,8 @@ def wahba_davenport(r_B, r_N_real):
     D = np.zeros((4, 4)) # Initialize Davenport matrix
     for i in range(sensors):
         D = D + w[i]*adcs.L(adcs.H@r_N_real[:, i]).T@adcs.R(adcs.H@r_B[:, i])
-    val, vec = np.linalg.eig(D)
-    q_dav = vec[:, np.argmax(val)]
+    val, vec = np.linalg.eigh(D)
+    q_dav = vec[:, -1]
     Q_dav = adcs.Q(q_dav)
     return Q_dav, q_dav
 
@@ -90,43 +99,47 @@ def dsdq(residual, q_gn, tol):
         dx[i] = tol
         jacob[:, i] = (residual(q_gn + dx) - residual(q_gn - dx)) / (2 * tol)
     return jacob
-
-def qdiff(q_computed, q_real):
-    # Find the difference between two quaternions
-    if np.sign(q_real[0]) == np.sign(q_computed[0]):
-    # Same sign, subtract
-        return q_real - q_computed
-    else:
-    # Opposite signs, add
-        return q_real + q_computed
     
 def Qdiff(Q_computed, Q_real):
     # Find the difference in degrees between two rotation matrices
     return (180/math.pi)*np.linalg.norm(adcs.unhat(scipy.linalg.logm(Q_computed.T@Q_real)))
  
-
 ##### Part 3: Checking Attitude Sensor Covariance Matrix Statistics ################
-if False:
-    trials = 100
+if True:
+    trials = 2000
     error_SRU = np.zeros((trials, 3))
     error_CSS = np.zeros((trials, 3))
 
-    # Generate a random vector, add noise store the error difference
+    # Generate a random vector, add noise and store the error difference
     for i in range(trials):
-        r_B_true = np.random.randn(3)
+    ### CSS measurements ###
+        r_B_real = np.random.randn(3) # Generate random 3-D vector
+        r_B_real = adcs.unit_vec(r_B_real) # Normalize the true body frame vector
 
-        r_B_SRU = r_B_true + np.random.multivariate_normal(np.zeros(3), S_SRU)
-        r_B_CSS = r_B_true + np.random.multivariate_normal(np.zeros(3), S_CSS)
+        r_B_CSS = r_B_real + np.random.multivariate_normal(np.zeros(3), S_CSS) # Add sampled noise
+        r_B_CSS = adcs.unit_vec(r_B_CSS) # Normalize again
+        error_CSS[i] = r_B_CSS - r_B_real
 
-        error_SRU[i] = r_B_SRU - r_B_true
-        error_CSS[i] = r_B_CSS - r_B_true
+    ### SRU measurements ###
+        # q_real = np.random.randn(4) # Generate random quaternion
+        # q_real = q_real/np.linalg.norm(q_real) # Normalize to unit quaternion
+        q_real = adcs.qexp(r_B_real) # Use the same random vector as before, but cast to quaternion. Can also just generate a new random quaternion in the lines above but idk
+
+        noise_vec = np.random.multivariate_normal(np.zeros(3), S_SRU) # This is a 3-parameter rotation
+        delta_q = adcs.qexp(noise_vec) # Turn the noise vector to a noise delta quaternion
+        q_SRU = adcs.qmult(q_real, delta_q) # Quaternion multiply on the error
+        q_error = adcs.qmult(q_SRU, adcs.qinv(q_real))
+        error_SRU[i] = 2*q_error[1:]
     
     covar_SRU = np.cov(error_SRU.T) # should be close to S_SRU
     covar_CSS = np.cov(error_CSS.T) # should be close to S_CSS
 
+    print(covar_SRU - S_SRU)
+    print(covar_CSS - S_CSS)
+
 ##### Part 4: Wahba's Problem: Monte Carlo Methods ##################################
 if True:
-    trials = 30 # Number of runs
+    trials = 100 # Number of runs
     t_svd = np.zeros(trials)
     t_dav = np.zeros(trials)
     t_gn = np.zeros(trials)
@@ -136,14 +149,14 @@ if True:
     error_gn = np.zeros(trials)
 
     for i in range(trials):
-        r_B, r_N_real, Q_real, q_real = random_sensor(sensors)
+        r_B, r_N_real, Q_real, q_real = random_sensor(2, 2)
 
         start_svd = time.perf_counter()
         Q_svd, q_svd = wahba_svd(r_B, r_N_real)
         t_svd[i] = time.perf_counter() - start_svd
 
         start_dav = time.perf_counter()
-        Q_dav, q_dav = wahba_svd(r_B, r_N_real)
+        Q_dav, q_dav = wahba_davenport(r_B, r_N_real)
         t_dav[i] = time.perf_counter() - start_dav
 
         start_gn = time.perf_counter()
